3bcca59fa99f3217bd2dc7a62e93af629d47b9fb
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-02-26 13:13:21 -0500

    feat: created bun project

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { intro, outro, log } from "@clack/prompts";
+
+intro("Welcome to the Missive API!");
+log.info("made by @kieran");
+
+outro("Thanks for using the Missive API!\n have a great day!");
